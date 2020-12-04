d832b9d3b097bcd9584389c53e2580ab13c8a563
natalivrs <verasnatali4@gmail.com>
2020-12-04 18:06:59 +0100

    Movies separated from shows

--- script.js
+++ script.js
@@ -2,22 +2,40 @@ var app = new Vue({
   el: '#app',
   data: {
     // SEARCH
-    searchResults: [],
+    // searchResults: [],
+    moviesResults: [],
+    showsResults: [],
     userQuery: "",
     // LANGUAGE FLAGS AVAILABLE
-    flagsExist: 'afarbgcncsdadeenesetfrgahihuitjakonlnoplptroruskskslsvtrvi'
+    flagsExist: 'afarbgcncsdadeenesetfrgahihuitjakonlnoplptroruskslsvtrvi'
   },
   mounted: function () {
+    // FETCH MOVIES FROM API
     const movieInfo =
     "https://api.themoviedb.org/3/search/movie?api_key=149b8df650057fdf2402c5c032bf9560&language=en-US&query="
-     + "10+th" + "&page=1&include_adult=false"
+     + "10+t" + "&page=1&include_adult=true"
 
     axios.get(movieInfo)
     .then(movie => {
 
       for (var i = 0; i < movie.data.results.length; i++) {
 
-        this.searchResults.push(movie.data.results[i]);
+        this.moviesResults.push(movie.data.results[i]);
+
+      }
+
+    });
+
+    // FETCH TV SHOWS FROM API
+    const tvInfo = "https://api.themoviedb.org/3/search/tv?api_key=149b8df650057fdf2402c5c032bf9560&language=en-US&query="
+     + "10+t" + "&page=1&include_adult=true"
+
+    axios.get(tvInfo)
+    .then(shows => {
+
+      for (var i = 0; i < shows.data.results.length; i++) {
+
+        this.showsResults.push(shows.data.results[i]);
 
       }
 
@@ -44,16 +62,16 @@ var app = new Vue({
 
       });
 
-      // FETCH TV SERIES FROM API
+      // FETCH TV SHOWS FROM API
       const tvInfo = "https://api.themoviedb.org/3/search/tv?api_key=149b8df650057fdf2402c5c032bf9560&language=en-US&query="
        + this.userQuery + "&page=1&include_adult=true"
 
       axios.get(tvInfo)
-      .then(series => {
+      .then(shows => {
 
-        for (var i = 0; i < series.data.results.length; i++) {
+        for (var i = 0; i < shows.data.results.length; i++) {
 
-          this.searchResults.push(series.data.results[i]);
+          this.searchResults.push(shows.data.results[i]);
 
         }
 
@@ -65,23 +83,30 @@ var app = new Vue({
     // FIND LANGUAGE FLAG
     findFlag: function (lang) {
 
-      if (this.flagsExist.includes(this.searchResults[lang].original_language)) {
-        return  "img/flag-lang/" + this.searchResults[lang].original_language + ".png"
+      if (this.flagsExist.includes(this.moviesResults[lang].original_language)) {
+        return  "img/flag-lang/" + this.moviesResults[lang].original_language + ".png"
       } else {
         return  "img/flag-lang/world.png"
       }
 
     },
-    // FIND MOVIE/SERIES POSTER
-    findPoster: function (index) {
+    // FIND MOVIE/SHOWS POSTER
+    findPoster: function (index, match) {
 
-      return "https://image.tmdb.org/t/p/" + "w342" + this.searchResults[index].poster_path
+
+      if (match.first_air_date) {
+        return "https://image.tmdb.org/t/p/" + "w342" + this.showsResults[index].poster_path
+      } else {
+        return "https://image.tmdb.org/t/p/" + "w342" + this.moviesResults[index].poster_path
+      }
+
 
 
     },
+    // SMALL IMAGE
     findBackDrop: function (index) {
 
-      return "https://image.tmdb.org/t/p/" + "w780" + this.searchResults[index].backdrop_path
+      return "https://image.tmdb.org/t/p/" + "w780" + this.moviesResults[index].backdrop_path
 
     }
 
